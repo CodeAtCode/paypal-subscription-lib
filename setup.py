dbf1bd9d219d0afc4ddadab347df134d83713afc
Daniele Scasciafratte <mte90net@gmail.com>
2025-04-14 18:16:29 +0200

    feat(bump): done ♬ Power Glove - Revenge

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paypal-subscription',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='Codeat',
     author_email='mte90net@gmail.com',
